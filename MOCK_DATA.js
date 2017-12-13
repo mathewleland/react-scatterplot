60e660f1c75d60dec4b001cb4e84c51afe1358a4
mathewleland <mathewhleland@gmail.com>
2017-12-12 23:08:21 -0800

    fake data to plot on the graph

---
+++ MOCK_DATA.js
@@ -0,0 +1,338 @@
+module.exports = [
+  {
+    "start_time": "2017-11-27 22:29:19",
+    "status": 'error',
+    "duration": 183
+  },
+  {
+    "start_time": "2017-11-25 11:58:11",
+    "status": 'fail',
+    "duration": 170
+  },
+  {
+    "start_time": "2017-11-23 03:42:30",
+    "status": 'fail',
+    "duration": 128
+  },
+  {
+    "start_time": "2017-11-25 16:03:50",
+    "status": 'fail',
+    "duration": 235
+  },
+  {
+    "start_time": "2017-11-25 02:26:13",
+    "status": 'pass',
+    "duration": 112
+  },
+  {
+    "start_time": "2017-11-24 11:26:31",
+    "status": 'pass',
+    "duration": 292
+  },
+  {
+    "start_time": "2017-11-25 12:25:37",
+    "status": 'pass',
+    "duration": 300
+  },
+  {
+    "start_time": "2017-11-23 01:02:54",
+    "status": 'pass',
+    "duration": 89
+  },
+  {
+    "start_time": "2017-11-23 17:13:47",
+    "status": 'pass',
+    "duration": 81
+  },
+  {
+    "start_time": "2017-11-26 16:59:58",
+    "status": 'error',
+    "duration": 78
+  },
+  {
+    "start_time": "2017-11-22 09:07:00",
+    "status": 'pass',
+    "duration": 267
+  },
+  {
+    "start_time": "2017-11-23 21:55:38",
+    "status": 'error',
+    "duration": 139
+  },
+  {
+    "start_time": "2017-11-24 10:34:38",
+    "status": 'pass',
+    "duration": 238
+  },
+  {
+    "start_time": "2017-11-25 02:49:52",
+    "status": 'pass',
+    "duration": 216
+  },
+  {
+    "start_time": "2017-11-28 14:19:36",
+    "status": 'fail',
+    "duration": 129
+  },
+  {
+    "start_time": "2017-11-26 04:33:06",
+    "status": 'pass',
+    "duration": 141
+  },
+  {
+    "start_time": "2017-11-22 21:37:28",
+    "status": 'fail',
+    "duration": 208
+  },
+  {
+    "start_time": "2017-11-22 01:10:15",
+    "status": 'pass',
+    "duration": 252
+  },
+  {
+    "start_time": "2017-11-23 13:17:58",
+    "status": 'fail',
+    "duration": 158
+  },
+  {
+    "start_time": "2017-11-25 02:11:26",
+    "status": 'error',
+    "duration": 158
+  },
+  {
+    "start_time": "2017-11-28 12:17:21",
+    "status": 'fail',
+    "duration": 234
+  },
+  {
+    "start_time": "2017-11-22 13:36:33",
+    "status": 'pass',
+    "duration": 118
+  },
+  {
+    "start_time": "2017-11-22 10:40:10",
+    "status": 'pass',
+    "duration": 248
+  },
+  {
+    "start_time": "2017-11-27 17:35:36",
+    "status": 'pass',
+    "duration": 276
+  },
+  {
+    "start_time": "2017-11-25 18:37:20",
+    "status": 'pass',
+    "duration": 170
+  },
+  {
+    "start_time": "2017-11-27 23:03:51",
+    "status": 'pass',
+    "duration": 205
+  },
+  {
+    "start_time": "2017-11-23 19:27:43",
+    "status": 'pass',
+    "duration": 184
+  },
+  {
+    "start_time": "2017-11-22 13:34:51",
+    "status": 'pass',
+    "duration": 154
+  },
+  {
+    "start_time": "2017-11-28 16:13:10",
+    "status": 'pass',
+    "duration": 154
+  },
+  {
+    "start_time": "2017-11-28 04:43:39",
+    "status": 'fail',
+    "duration": 209
+  },
+  {
+    "start_time": "2017-11-27 12:03:07",
+    "status": 'pass',
+    "duration": 92
+  },
+  {
+    "start_time": "2017-11-24 21:57:16",
+    "status": 'pass',
+    "duration": 238
+  },
+  {
+    "start_time": "2017-11-26 05:10:46",
+    "status": 'pass',
+    "duration": 235
+  },
+  {
+    "start_time": "2017-11-24 12:51:59",
+    "status": 'pass',
+    "duration": 273
+  },
+  {
+    "start_time": "2017-11-23 00:10:26",
+    "status": 'error',
+    "duration": 148
+  },
+  {
+    "start_time": "2017-11-24 01:26:09",
+    "status": 'error',
+    "duration": 271
+  },
+  {
+    "start_time": "2017-11-28 08:09:44",
+    "status": 'fail',
+    "duration": 167
+  },
+  {
+    "start_time": "2017-11-26 08:41:43",
+    "status": 'pass',
+    "duration": 175
+  },
+  {
+    "start_time": "2017-11-25 11:29:32",
+    "status": 'pass',
+    "duration": 209
+  },
+  {
+    "start_time": "2017-11-22 00:04:02",
+    "status": 'pass',
+    "duration": 241
+  },
+  {
+    "start_time": "2017-11-23 18:30:36",
+    "status": 'pass',
+    "duration": 112
+  },
+  {
+    "start_time": "2017-11-22 04:26:03",
+    "status": 'fail',
+    "duration": 132
+  },
+  {
+    "start_time": "2017-11-28 11:56:44",
+    "status": 'pass',
+    "duration": 242
+  },
+  {
+    "start_time": "2017-11-23 03:51:19",
+    "status": 'pass',
+    "duration": 280
+  },
+  {
+    "start_time": "2017-11-24 06:17:49",
+    "status": 'pass',
+    "duration": 163
+  },
+  {
+    "start_time": "2017-11-24 22:35:59",
+    "status": 'fail',
+    "duration": 202
+  },
+  {
+    "start_time": "2017-11-27 10:45:46",
+    "status": 'pass',
+    "duration": 267
+  },
+  {
+    "start_time": "2017-11-26 21:02:47",
+    "status": 'pass',
+    "duration": 140
+  },
+  {
+    "start_time": "2017-11-26 21:57:00",
+    "status": 'pass',
+    "duration": 234
+  },
+  {
+    "start_time": "2017-11-27 21:39:34",
+    "status": 'error',
+    "duration": 300
+  },
+  {
+    "start_time": "2017-11-23 02:57:46",
+    "status": 'pass',
+    "duration": 191
+  },
+  {
+    "start_time": "2017-11-22 01:25:46",
+    "status": 'pass',
+    "duration": 260
+  },
+  {
+    "start_time": "2017-11-27 23:09:00",
+    "status": 'pass',
+    "duration": 240
+  },
+  {
+    "start_time": "2017-11-25 17:16:00",
+    "status": 'error',
+    "duration": 92
+  },
+  {
+    "start_time": "2017-11-26 10:36:41",
+    "status": 'pass',
+    "duration": 169
+  },
+  {
+    "start_time": "2017-11-23 09:12:21",
+    "status": 'error',
+    "duration": 106
+  },
+  {
+    "start_time": "2017-11-28 22:54:56",
+    "status": 'pass',
+    "duration": 266
+  },
+  {
+    "start_time": "2017-11-26 05:48:13",
+    "status": 'pass',
+    "duration": 289
+  },
+  {
+    "start_time": "2017-11-23 21:09:31",
+    "status": 'error',
+    "duration": 250
+  },
+  {
+    "start_time": "2017-11-26 09:46:24",
+    "status": 'pass',
+    "duration": 285
+  },
+  {
+    "start_time": "2017-11-28 18:32:21",
+    "status": 'error',
+    "duration": 260
+  },
+  {
+    "start_time": "2017-11-25 11:29:55",
+    "status": 'error',
+    "duration": 218
+  },
+  {
+    "start_time": "2017-11-25 07:21:51",
+    "status": 'fail',
+    "duration": 274
+  },
+  {
+    "start_time": "2017-11-26 06:55:55",
+    "status": 'pass',
+    "duration": 112
+  },
+  {
+    "start_time": "2017-11-22 00:06:21",
+    "status": 'pass',
+    "duration": 111
+  },
+  {
+    "start_time": "2017-11-25 13:40:19",
+    "status": 'pass',
+    "duration": 211
+  },
+  {
+    "start_time": "2017-11-25 13:53:38",
+    "status": 'pass',
+    "duration": 271
+  }
+
+]
